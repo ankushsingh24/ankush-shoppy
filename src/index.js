eb80c78a45b000676c96a40edebc9426fab2a286
Ankush Singh <ankushsahil45@gmail.com>
2022-07-20 23:12:43 +0530

    license

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+// Registering Syncfusion license key
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VVhiQlFadVlJVXxId0x0RWFbb196dFJMYVRBJAtUQF1hS35UdEViWnxbc3VQR2lV"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
