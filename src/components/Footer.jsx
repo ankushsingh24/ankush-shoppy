64fe27e52a54e10f143787bc17949a5aca693d84
Ankush Singh <ankushsahil45@gmail.com>
2022-07-21 19:51:47 +0530

    added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="mt-24">
+        <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+          © 2022 All rights reserved by Shoppy.com
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
