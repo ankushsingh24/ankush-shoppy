9221ff90086aa4c2b6903ba3f2238dcfdc4e6c75
Ankush Singh <ankushsahil45@gmail.com>
2022-07-21 22:16:39 +0530

    added chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MdOutlineCancel } from "react-icons/md";
 
 import { Button } from ".";
-import { ChartData } from "../data/dummy";
+import { chatData } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Chat = () => {
