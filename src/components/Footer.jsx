e25c503cfb3f052932bfb8c0d78586d809c5c386
Ankush Singh <ankushsahil45@gmail.com>
2022-07-21 20:40:24 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <div className="mt-24">
         <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-          © 2022 All rights reserved by Shoppy.com
+          © 2022 All rights reserved by Ankush-Shoppy.com
         </p>
       </div>
     </div>
