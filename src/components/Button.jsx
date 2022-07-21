ac03a4d60d173bf478e9af44f51cb3f96585c608
Ankush Singh <ankushsahil45@gmail.com>
2022-07-21 20:50:33 +0530

    updated button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useStateContext } from "../../context/ContextProvider";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Button = ({
   icon,
