d94ca782a0998c6bf6a425d3b4e93e865e4b9ce2
Ankush Singh <ankushsahil45@gmail.com>
2022-07-23 23:43:28 +0530

    bug fixed

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -9,7 +9,7 @@ const Notification = () => {
   const { currentColor } = useStateContext();
 
   return (
-    <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#4246D] p-8 rounded-lg w-96">
+    <div className="nav-item absolute right-5 md:right-40 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
         <div className="flex gap-3">
           <p className="font-semibold text-lg dark:text-gray-200">
@@ -17,7 +17,7 @@ const Notification = () => {
           </p>
           <button
             type="button"
-            className="text-white text-xs rounded p-1 px-2 bg-orange-theme"
+            className="text-white text-xs rounded p-1 px-2 bg-orange-theme "
           >
             {" "}
             5 New
