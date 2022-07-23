587a3a2c38f58e699e622ac0a17bdff6768e7d35
Ankush Singh <ankushsahil45@gmail.com>
2022-07-23 21:18:50 +0530

    added editor

---
+++ src/pages/Editor.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { Header } from "../components";
+
+const Editor = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header />
+      <RichTextEditorComponent>
+        <Inject services={[Image, Link, HtmlEditor, QuickToolbar, Toolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
+};
+
+export default Editor;
