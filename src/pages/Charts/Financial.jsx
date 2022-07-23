729e7b6d69e5aa7854976d3140d2949b3403ff4d
Ankush Singh <ankushsahil45@gmail.com>
2022-07-24 00:30:53 +0530

    bug

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -34,7 +34,7 @@ const Financial = () => {
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <ChartsHeader category="Financial" title="AAPLE Historical" />
+      <ChartsHeader category="Financial" title="APPLE Historical" />
       <div className="w-full">
         <ChartComponent
           id="charts"
