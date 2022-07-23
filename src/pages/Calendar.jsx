cf75e915f9b0bc6e945eee3d366eb916408d9c5c
Ankush Singh <ankushsahil45@gmail.com>
2022-07-23 21:28:04 +0530

    updated title

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  ScheduleComponent,
   Day,
   Week,
   WorkWeek,
@@ -10,10 +11,10 @@ import {
   DragAndDrop,
 } from "@syncfusion/ej2-react-schedule";
 
-import { scheduleData } from "./data/dummy";
+import { scheduleData } from "../data/dummy";
 import { Header } from "../components";
 
-const Calender = () => {
+const Calendar = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calendar" />
@@ -29,3 +30,5 @@ const Calender = () => {
     </div>
   );
 };
+
+export default Calendar;
