06326197251ffc458cb5acb62545ac13cdff02e0
Ankush Singh <ankushsahil45@gmail.com>
2022-07-23 21:28:21 +0530

    fixed bug

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { GoPrimitiveDot } from "react-icons/go";
-import { IoIoMore } from "react-icons/io";
+import { IoIosMore } from "react-icons/io";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 
 import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
@@ -16,7 +16,7 @@ import {
 import { useStateContext } from "../contexts/ContextProvider";
 import product9 from "../data/product9.jpg";
 
-const DropDown = () => {
+const DropDown = ({ currentMode }) => {
   <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
     <DropDownListComponent
       dataSource={dropdownData}
