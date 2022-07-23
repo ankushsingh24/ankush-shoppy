ccbeb7cabd22c06647eba4d354b222ba0477fecf
Ankush Singh <ankushsahil45@gmail.com>
2022-07-23 20:38:25 +0530

    earning section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -143,6 +143,37 @@ const Ecommerce = () => {
             </div>
           </div>
         </div>
+
+        {/* -----------Earning SECTION-under revenue------------ */}
+        <div>
+          <div
+            className=" rounded-2xl md:w-400 p-4 m-3"
+            style={{ backgroundColor: currentColor }}
+          >
+            <div className="flex justify-between items-center ">
+              <p className="font-semibold text-white text-2xl">Earnings</p>
+              <div>
+                <p className="text-2xl text-white font-semibold mt-8">
+                  $63,448.78
+                </p>
+                <p className="text-gray-200">Monthly revenue</p>
+              </div>
+            </div>
+
+            <div className="mt-4">
+              {/* SPARKLINE */}
+              <SparkLine
+                currentColor={currentColor}
+                id="column-sparkLine"
+                height="100px"
+                type="Column"
+                data={SparklineAreaData}
+                width="320"
+                color="rgb(242, 252, 253)"
+              />
+            </div>
+          </div>
+
       </div>
     </div>
   );
